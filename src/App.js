1db339d939f42ae940d7059eb6c3bae3b7da1aea
Davy Peres <otaldodavy@gmail.com>
2024-11-08 23:40:31 -0300

    Pagina Principal Feita (ainda nao fiz responsivo)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import './style.css'
+
+import Header from "./components/Header/Header";
+import Logo from "./components/Logo/Logo";
+import BarraDePesquisa from './components/BarraDePesquisa/BarraDePesquisa';
+import EscolhaOPersonagem from './components/EscolhaOPersonagem/EscolhaOPersonagem';
+
+
 function App() {
+  const paginaPrincipal = true
   return (
-    <div>
-    </div>
+    // Pagina Inicial
+    paginaPrincipal ? <>
+    <div className='pagina-principal'>
+      <Header />
+      <Logo />
+      <BarraDePesquisa />
+      <EscolhaOPersonagem />
+    </div></> : 
+    // Pagina quando for pesquisar
+    <>
+    </>
   );
 }
 
