25b2f6d271d06e33b0ddc0c100ce591e5e770217
Davy Peres <otaldodavy@gmail.com>
2024-11-13 21:15:07 -0300

    mostrando resultados porem ainda nao aprimorado a barra de pesquisa

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ import imgDark from "./images/fundo-tema-dark.jpeg";
 import imgLight from "./images/fundo-tema-light.jpeg";
 
 import { useState } from "react"
+import Resultados from './components/Resultados/Resultados.tsx';
 
 function App() {
-  const paginaPrincipal = true
   // Tema Dark e Light
   const [temaAtual, setTemaAtual] = useState('Dark')
   const [background, setBackground] = useState(imgDark)
@@ -27,20 +27,50 @@ function App() {
       lado === "direito" ? setLado("esquerdo") : setLado("direito")
   }
 // ------------------------------------------------------
+
+//Saindo da Pagina inicial
+  const [paginaPrincipal, setPaginaPrincipal] = useState(1)
+  const updatePagina = () => {
+    paginaPrincipal === 1 ? setPaginaPrincipal(2) : console.log()
+  }
+
+// -------------------------------------------------------
+
+const [searchQuery, setSearchQuery] = useState(""); 
+
+  // Função que será chamada quando a pesquisa mudar
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+  };
+
+
   return (
     // Pagina Inicial
-    paginaPrincipal ? <>
+    paginaPrincipal === 1 ? <>
     <div style={{backgroundImage: `url(${background})`, color: `${color}`}} className='pagina-principal'>
         <header className={'header' + (lado === "esquerdo" ? " direito" : "")}>
             <img style={{filter: `${temaicon}`}} src={tema} alt="Icone Tema" className="imgTema"/>
             <button style={{color: `${color}`}} className="mudartema" onClick={() => {mudarTema()}}>Tema {temaAtual} <p>mudar tema</p></button>
         </header>
-      <Logo />
-      <BarraDePesquisa />
+        <Logo logo={(paginaPrincipal === 2 ? "imgLogo-2" : "imgLogo-1")}/>
+      <BarraDePesquisa onSearch={handleSearch} handlePrincipal={updatePagina} pagina={(paginaPrincipal === 2 ? "barradepesquisa-2" : "barradepesquisa-1")} botao={(paginaPrincipal === 2 ? "botaolupaprocurar-2" : "botaolupaprocurar-1")}
+      lupa={(paginaPrincipal === 2 ? "lupa-2" : "lupa-1")}/>
       <EscolhaOPersonagem />
-    </div></> : 
+    </div></> : paginaPrincipal === 2 ?
     // Pagina quando for pesquisar
     <>
+    <div style={{backgroundImage: `url(${background})`, color: `${color}`}} className='pagina-principal'>
+        <header className={'header esquerdo'}>
+            <img style={{filter: `${temaicon}`}} src={tema} alt="Icone Tema" className="imgTema"/>
+            <button style={{color: `${color}`}} className="mudartema" onClick={() => {mudarTema()}}>Tema {temaAtual} <p>mudar tema</p></button>
+        </header>
+      <Logo logo={(paginaPrincipal === 2 ? "imgLogo-2" : "imgLogo-1")}/>
+      <BarraDePesquisa onSearch={handleSearch} handlePrincipal={updatePagina} pagina={(paginaPrincipal === 2 ? "barradepesquisa-2" : "barradepesquisa-1")} botao={(paginaPrincipal === 2 ? "botaolupaprocurar-2" : "botaolupaprocurar-1")}
+      lupa={(paginaPrincipal === 2 ? "lupa-2" : "lupa-1")}/>
+
+      <Resultados searchQuery={searchQuery}/>
+    </div>
+    </> : <>
     </>
   );
 }
